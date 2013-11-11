1414cb114724eb249d4d0789957c9cb62c6435d1
Hariharan Subramanian <hariharan.subramanian@orbitz.com>
2013-11-11 14:55:25 -0600

    fix space in input and updated UI

--- src/com/orbitz/weather/activity/WeatherActivity.java
+++ src/com/orbitz/weather/activity/WeatherActivity.java
@@ -23,6 +23,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public class WeatherActivity extends Activity {
@@ -44,16 +46,21 @@ public class WeatherActivity extends Activity {
         bindViews();
     }
 
-    public void search(View view) {
+    public void search(View view) throws URISyntaxException {
         String location = locationInput.getText().toString();
-        String url = "http://api.worldweatheronline.com/premium/v1/weather.ashx?q=" +
-                location + "&format=json&fx=no&key=8cgvkzqjjcmk7wnsag822n3y";
+        URI uri = new URI(
+                "http",
+                "api.worldweatheronline.com",
+                "/premium/v1/weather.ashx",
+                "q=" + location + "&format=json&fx=no&key=8cgvkzqjjcmk7wnsag822n3y",
+                null);
+
 
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
         if (networkInfo != null && networkInfo.isConnected()) {
-            new WeatherService().execute(url);
+            new WeatherService().execute(uri.toString());
         }
 
     }
@@ -66,29 +73,30 @@ public class WeatherActivity extends Activity {
         description = (TextView) findViewById(R.id.description);
     }
 
-    private class WeatherService extends AsyncTask<String, Void, InputStream> {
+    private class WeatherService extends AsyncTask<String, Void, String> {
 
         @Override
-        protected InputStream doInBackground(String... url) {
+        protected String doInBackground(String... url) {
             try {
                 return getResponse(url[0]);
             } catch (IOException e)  {
                 //TODO
+                String exception = e.getStackTrace().toString();
                 return null;
             }
         }
 
         @Override
-        protected void onPostExecute(InputStream result) {
+        protected void onPostExecute(String result) {
             initializeView(result);
         }
     }
 
-    private class WeatherIconService extends AsyncTask<String, Void, InputStream> {
+    private class WeatherIconService extends AsyncTask<String, Void, Bitmap> {
         @Override
-        protected InputStream doInBackground(String... url) {
+        protected Bitmap doInBackground(String... url) {
             try {
-                return getResponse(url[0]);
+                return getIconResponse(url[0]);
             } catch (IOException e)  {
                 //TODO
                 return null;
@@ -96,26 +104,20 @@ public class WeatherActivity extends Activity {
         }
 
         @Override
-        protected void onPostExecute(InputStream result) {
+        protected void onPostExecute(Bitmap result) {
             initializeIcon(result);
         }
     }
 
-    private InputStream getResponse(String requestUrl) throws IOException {
+    private Bitmap getIconResponse(String requestUrl) throws IOException {
         InputStream is = null;
         try {
-            URL url = new URL(requestUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setReadTimeout(1000);
-            connection.setConnectTimeout(15000);
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Accept-Encoding", "");
-            connection.setDoInput(true);
-            connection.connect();
-
+            HttpURLConnection connection = getConnection(requestUrl);
             is = connection.getInputStream();
 
-            return is;
+            Bitmap bitmap = BitmapFactory.decodeStream(is);
+
+            return bitmap;
 
         } catch (Exception e) {
             return null;
@@ -127,8 +129,13 @@ public class WeatherActivity extends Activity {
         }
     }
 
-    private void initializeView(InputStream is) {
+    private String getResponse(String requestUrl) throws IOException {
+        InputStream is = null;
         try {
+
+            HttpURLConnection connection = getConnection(requestUrl);
+            is = connection.getInputStream();
+
             BufferedReader in = new BufferedReader(new InputStreamReader(is));
 
             StringBuilder builder = new StringBuilder();
@@ -137,8 +144,21 @@ public class WeatherActivity extends Activity {
                 builder.append(line);
             }
 
+            return builder.toString();
 
-            JSONObject json = new JSONObject(builder.toString());
+        } catch (Exception e) {
+            return null;
+        }
+        finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private void initializeView(String data) {
+        try {
+            JSONObject json = new JSONObject(data);
             JSONObject jsonData = (JSONObject) json.get("data");
             JSONArray current =  (JSONArray) jsonData.get("current_condition");
             JSONObject currentJSON = current.getJSONObject(0);
@@ -153,23 +173,34 @@ public class WeatherActivity extends Activity {
             JSONObject weatherIconObj = weatherIconArray.getJSONObject(0);
             String iconUrl = weatherIconObj.getString("value");
 
-
-
-
-
-
             currentTemp.setText(temp_f + " F");
-            precipitation.setText(precip + " mm");
+            precipitation.setText("Precipitation: " + precip + " mm");
 
             description.setText(weatherDesc);
+
+            new WeatherIconService().execute(iconUrl);
+
+
 
         } catch (Exception e) {
             //TODO
         }
     }
 
-    private void initializeIcon(InputStream is) {
-        Bitmap bitmap = BitmapFactory.decodeStream(is);
-        icon.setImageBitmap(bitmap);
+    private void initializeIcon(Bitmap bmp) {
+        icon.setImageBitmap(bmp);
+    }
+
+    private HttpURLConnection getConnection(String requestUrl) throws Exception {
+        URL url = new URL(requestUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setReadTimeout(1000);
+        connection.setConnectTimeout(15000);
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept-Encoding", "");
+        connection.setDoInput(true);
+        connection.connect();
+
+        return connection;
     }
 }
